84497408bf80754bfbc097b215201559efe7100f
Matthieu Lagacherie <matthieu.lagacherie@gmail.com>
2013-07-27 15:23:27 +0200

    update to last spark version

--- project.clj
+++ project.clj
@@ -8,14 +8,14 @@
 
   :jvm-opts ["-Dlog4j.configuration=file:log4j.properties"]
 
-  :dependencies [[org.clojure/clojure "1.4.0"]
+  :dependencies [[org.clojure/clojure "1.5.1"]
                  [org.clojure/tools.logging "0.2.3"]
                  [org.clojure/tools.cli "0.2.1"]
                  [org.clojars.mlimotte/serializable-fn "0.0.3"]]
 
   ; Include te Spark dep for compile and run, but not for uberjar
   :profiles {
-      :dev {:dependencies [[org.spark-project/spark-core_2.9.2 "0.6.1"]]}}
+      :dev {:dependencies [[org.spark-project/spark-core_2.9.3 "0.7.3"]]}}
 
   :aot [clj-spark.spark.functions
         clj-spark.api
